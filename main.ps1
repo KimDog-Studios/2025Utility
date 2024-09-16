6dcb57d03d81c5fdfdc7b65f527ac46e0042f478
KimDog Studios <kim.dog.543@gmail.com>
2024-09-16 11:55:43 +0100

    Update

--- main.ps1
+++ main.ps1
@@ -1,3 +1,17 @@
+# Check if the script is running as Administrator
+function Check-Admin {
+    $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    if (-not $isAdmin) {
+        Write-Warning "This script is not running as an Administrator."
+        # Relaunch the script with Administrator privileges
+        Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Definition)`"" -Verb RunAs
+        exit
+    }
+}
+
+# Run the admin check at the start of the script
+Check-Admin
+
 # URL of the winget menu script
 $wingetMenuUrl = "https://raw.githubusercontent.com/KimDog-Studios/2025Utility/main/functions/winget.ps1"
 
@@ -108,7 +122,6 @@ function Show-MainHeader {
     Draw-Box -Text "KimDog's Windows Utility | Last Updated: 2024-09-15"
     Write-Host "`n"  # Reduced gap
 }
-
 
 # Function to show the main menu
 function Show-MainMenu {

85e3ade402bde6cc1f704ace233fb91f4ac11454
KimDog Studios <kim.dog.543@gmail.com>
2024-09-18 00:13:07 +0100

    Ypdate

--- functions/Windows/Invoke-MouseAcceleration.ps1
+++ functions/Windows/Invoke-MouseAcceleration.ps1
@@ -1,38 +1,27 @@
-Function Invoke-WinUtilMouseAcceleration {
+function Invoke-WinUtilMouseAcceleration {
     <#
-
     .SYNOPSIS
-        Enables/Disables Mouse Acceleration
-
-    .PARAMETER DarkMoveEnabled
-        Indicates the current Mouse Acceleration State
+        Disables Mouse Acceleration
 
     #>
-    Param($MouseAccelerationEnabled)
     try {
-        if ($MouseAccelerationEnabled -eq $false) {
-            Write-Host "Enabling Mouse Acceleration"
-            $MouseSpeed = 1
-            $MouseThreshold1 = 6
-            $MouseThreshold2 = 10
-        } else {
-            Write-Host "Disabling Mouse Acceleration"
-            $MouseSpeed = 0
-            $MouseThreshold1 = 0
-            $MouseThreshold2 = 0
-
-        }
+        Write-Host "Disabling Mouse Acceleration..." -ForegroundColor Green
+        
+        $MouseSpeed = 0
+        $MouseThreshold1 = 0
+        $MouseThreshold2 = 0
 
         $Path = "HKCU:\Control Panel\Mouse"
         Set-ItemProperty -Path $Path -Name MouseSpeed -Value $MouseSpeed
         Set-ItemProperty -Path $Path -Name MouseThreshold1 -Value $MouseThreshold1
         Set-ItemProperty -Path $Path -Name MouseThreshold2 -Value $MouseThreshold2
+        
+        Write-Host "Mouse Acceleration has been disabled." -ForegroundColor Green
     } catch [System.Security.SecurityException] {
-        Write-Warning "Unable to set $Path\$Name to $Value due to a Security Exception"
+        Write-Warning "Unable to set mouse acceleration settings due to a Security Exception."
     } catch [System.Management.Automation.ItemNotFoundException] {
-        Write-Warning $psitem.Exception.ErrorRecord
+        Write-Warning "Registry path or key not found: $Path"
     } catch {
-        Write-Warning "Unable to set $Name due to unhandled exception"
-        Write-Warning $psitem.Exception.StackTrace
+        Write-Warning "An unexpected error occurred: ${_}"
     }
 }

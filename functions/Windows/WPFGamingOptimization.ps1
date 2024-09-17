2857618712af99ffc0347ac7742d8db6190b4e2e
KimDog Studios <kim.dog.543@gmail.com>
2024-09-18 00:49:43 +0100

    Update

--- functions/Windows/WPFGamingOptimization.ps1
+++ functions/Windows/WPFGamingOptimization.ps1
@@ -1,20 +1,23 @@
 function Invoke-WinUtilDarkMode {
     <#
     .SYNOPSIS
-        Enables Dark Mode in Windows.
-
+        Enables Dark Mode in Windows if not already enabled.
     #>
     try {
-        Write-Host "Enabling Dark Mode..." -ForegroundColor Green
+        Write-Host "Checking Dark Mode status..." -ForegroundColor Green
         
-        $DarkMoveValue = 0  # 0 for Dark Mode, 1 for Light Mode
-
+        $DarkModeValue = 0  # 0 for Dark Mode, 1 for Light Mode
         $Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-        Set-ItemProperty -Path $Path -Name AppsUseLightTheme -Value $DarkMoveValue
-        Set-ItemProperty -Path $Path -Name SystemUsesLightTheme -Value $DarkMoveValue
-        
-        Write-Host "Dark Mode has been enabled." -ForegroundColor Green
-        
+        $currentDarkMode = (Get-ItemProperty -Path $Path -Name AppsUseLightTheme).AppsUseLightTheme
+
+        if ($currentDarkMode -ne $DarkModeValue) {
+            Write-Host "Enabling Dark Mode..." -ForegroundColor Green
+            Set-ItemProperty -Path $Path -Name AppsUseLightTheme -Value $DarkModeValue
+            Set-ItemProperty -Path $Path -Name SystemUsesLightTheme -Value $DarkModeValue
+            Write-Host "Dark Mode has been enabled." -ForegroundColor Green
+        } else {
+            Write-Host "Dark Mode is already enabled." -ForegroundColor Yellow
+        }
     } catch [System.Security.SecurityException] {
         Write-Warning "Unable to modify Dark Mode settings due to a Security Exception."
     } catch [System.Management.Automation.ItemNotFoundException] {
@@ -27,10 +30,10 @@ function Invoke-WinUtilDarkMode {
 function Invoke-WinUtilMouseAcceleration {
     <#
     .SYNOPSIS
-        Disables Mouse Acceleration and Enhance Pointer Precision, and waits for user confirmation.
+        Disables Mouse Acceleration and Enhance Pointer Precision if not already disabled.
     #>
     try {
-        Write-Host "Disabling Mouse Acceleration and Enhance Pointer Precision..." -ForegroundColor Green
+        Write-Host "Checking Mouse Acceleration settings..." -ForegroundColor Green
         
         $MouseSpeed = 0
         $MouseThreshold1 = 0
@@ -38,22 +41,24 @@ function Invoke-WinUtilMouseAcceleration {
         $EnhancePointerPrecision = 0  # 0 to disable, 1 to enable
 
         $Path = "HKCU:\Control Panel\Mouse"
-        Set-ItemProperty -Path $Path -Name MouseSpeed -Value $MouseSpeed
-        Set-ItemProperty -Path $Path -Name MouseThreshold1 -Value $MouseThreshold1
-        Set-ItemProperty -Path $Path -Name MouseThreshold2 -Value $MouseThreshold2
-        Set-ItemProperty -Path $Path -Name MouseEnhancePointerPrecision -Value $EnhancePointerPrecision
-        
-        # Confirm changes
         $currentSettings = Get-ItemProperty -Path $Path
-        Write-Host "Current Mouse Acceleration Settings:"
-        Write-Host "MouseSpeed: $($currentSettings.MouseSpeed)"
-        Write-Host "MouseThreshold1: $($currentSettings.MouseThreshold1)"
-        Write-Host "MouseThreshold2: $($currentSettings.MouseThreshold2)"
-        Write-Host "EnhancePointerPrecision: $($currentSettings.MouseEnhancePointerPrecision)"
-        
-        Write-Host "Mouse Acceleration and Enhance Pointer Precision have been disabled." -ForegroundColor Green
-        Write-Host "You might need to restart your computer or log out and log back in for changes to take effect."
-        
+
+        if ($currentSettings.MouseSpeed -ne $MouseSpeed -or 
+            $currentSettings.MouseThreshold1 -ne $MouseThreshold1 -or
+            $currentSettings.MouseThreshold2 -ne $MouseThreshold2 -or
+            $currentSettings.MouseEnhancePointerPrecision -ne $EnhancePointerPrecision) {
+            
+            Write-Host "Disabling Mouse Acceleration and Enhance Pointer Precision..." -ForegroundColor Green
+            Set-ItemProperty -Path $Path -Name MouseSpeed -Value $MouseSpeed
+            Set-ItemProperty -Path $Path -Name MouseThreshold1 -Value $MouseThreshold1
+            Set-ItemProperty -Path $Path -Name MouseThreshold2 -Value $MouseThreshold2
+            Set-ItemProperty -Path $Path -Name MouseEnhancePointerPrecision -Value $EnhancePointerPrecision
+
+            Write-Host "Mouse Acceleration and Enhance Pointer Precision have been disabled." -ForegroundColor Green
+            Write-Host "You might need to restart your computer or log out and log back in for changes to take effect."
+        } else {
+            Write-Host "Mouse Acceleration and Enhance Pointer Precision are already disabled." -ForegroundColor Yellow
+        }
     } catch [System.Security.SecurityException] {
         Write-Warning "Unable to set mouse acceleration settings due to a Security Exception."
     } catch [System.Management.Automation.ItemNotFoundException] {
@@ -62,7 +67,6 @@ function Invoke-WinUtilMouseAcceleration {
         Write-Warning "An unexpected error occurred: ${_}"
     }
 }
-
 
 function Invoke-WPFUltimatePerformance {
     <#
@@ -91,7 +95,7 @@ function Invoke-WPFUltimatePerformance {
 
         if ($State -eq "Enable") {
             if (-not (Is-UltimatePerformancePlanInstalled)) {
-                # Duplicate the Ultimate Performance power plan using its GUID
+                Write-Host "Duplicating Ultimate Performance power plan..." -ForegroundColor Green
                 $duplicateOutput = powercfg /duplicatescheme $ultimateGUID
 
                 $guid = $null
@@ -101,7 +105,7 @@ function Invoke-WPFUltimatePerformance {
                 # Extract the new GUID from the duplicateOutput
                 foreach ($line in $duplicateOutput) {
                     if ($line -match "\b[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\b") {
-                        $guid = $matches[0]  # $matches[0] will contain the first match, which is the GUID
+                        $guid = $matches[0]
                         Write-Output "GUID: $guid has been extracted and stored in the variable."
                         break
                     }
@@ -112,28 +116,26 @@ function Invoke-WPFUltimatePerformance {
                     exit 1
                 }
 
-                # Change the name of the power plan and set its description
+                Write-Host "Changing power plan name and description..." -ForegroundColor Green
                 $changeNameOutput = powercfg /changename $guid "$nameFromFile" "$description"
-                Write-Output "The power plan name and description have been changed. Output:"
                 Write-Output $changeNameOutput
 
-                # Set the duplicated Ultimate Performance plan as active
+                Write-Host "Setting power plan as active..." -ForegroundColor Green
                 $setActiveOutput = powercfg /setactive $guid
-                Write-Output "The power plan has been set as active. Output:"
                 Write-Output $setActiveOutput
 
-                # Set the display timeout to 0 (never turn off)
+                Write-Host "Setting display timeout to never turn off..." -ForegroundColor Green
                 powercfg /change monitor-timeout-ac 0
                 powercfg /change monitor-timeout-dc 0
 
                 Write-Host "> Ultimate Performance plan installed and set as active."
                 Write-Host "> Display timeout has been set to never turn off."
             } else {
-                Write-Host "Ultimate Performance plan is already installed and active."
+                Write-Host "Ultimate Performance plan is already installed and active." -ForegroundColor Yellow
             }
         } elseif ($State -eq "Disable") {
             if (Is-UltimatePerformancePlanInstalled) {
-                # Find the GUID of the Ultimate Performance plan
+                Write-Host "Uninstalling Ultimate Performance power plan..." -ForegroundColor Green
                 $installedPlan = powercfg -list | Select-String -Pattern $ultimatePlanName
                 if ($installedPlan) {
                     $ultimatePlanGUID = ($installedPlan.Line -split '\s+')[3]
@@ -148,17 +150,16 @@ function Invoke-WPFUltimatePerformance {
                     Write-Host "Ultimate Performance plan has been uninstalled."
                     Write-Host "> Balanced plan is now active."
                 } else {
-                    Write-Host "Ultimate Performance plan is not installed."
+                    Write-Host "Ultimate Performance plan is not installed." -ForegroundColor Yellow
                 }
             } else {
-                Write-Host "Ultimate Performance plan is not installed."
+                Write-Host "Ultimate Performance plan is not installed." -ForegroundColor Yellow
             }
         }
     } catch {
         Write-Error "Error occurred: $_"
     }
 }
-
 
 # Invoke the functions
 Invoke-WinUtilDarkMode

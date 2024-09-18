df8e6e80f028f6118663313ef410818f355b459e
KimDog Studios <kim.dog.543@gmail.com>
2024-09-18 20:07:06 +0100

    Update

--- functions/WindowsUtility/WPFStarter.ps1
+++ functions/WindowsUtility/WPFStarter.ps1
@@ -11,62 +11,6 @@ function Test-PackageManagerInstalled {
     }
 }
 
-# Optimize Create-Shortcut function
-function Create-Shortcut {
-    param(
-        [string]$ShortcutName,
-        [string]$ShortcutPath,
-        [string]$TargetPath,
-        [string]$Arguments = "",
-        [bool]$RunAsAdmin = $true
-    )
-
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
-    $Shortcut.TargetPath = $TargetPath
-    $Shortcut.Arguments = $Arguments
-
-    $iconUrl = "https://raw.githubusercontent.com/KimDog-Studios/2025Utility/main/icon.ico"
-    $iconPath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "logo.ico")
-    Invoke-WebRequest -Uri $iconUrl -OutFile $iconPath
-    $Shortcut.IconLocation = $iconPath
-
-    $Shortcut.Save()
-
-    if ($RunAsAdmin) {
-        $bytes = [System.IO.File]::ReadAllBytes($ShortcutPath)
-        $bytes[0x15] = $bytes[0x15] -bor 0x20
-        [System.IO.File]::WriteAllBytes($ShortcutPath, $bytes)
-    }
-
-    Write-Host "Shortcut '$ShortcutName' created at $ShortcutPath (Run as admin: $RunAsAdmin)"
-}
-
-# Combine Create-ShortcutInStartMenu and Create-DesktopShortcut
-function Create-WinUtilShortcuts {
-    $shell = if (Get-Command "pwsh" -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell.exe" }
-    $shellArgs = "-ExecutionPolicy Bypass -Command `"Start-Process $shell -verb runas -ArgumentList `'-Command `"irm https://bit.ly/4dcheD5 | iex`"`'"
-
-    $locations = @{
-        "Start Menu" = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('StartMenu'), 'Programs', 'KimDog Studios')
-        "Desktop" = [System.Environment]::GetFolderPath('Desktop')
-    }
-
-    foreach ($location in $locations.GetEnumerator()) {
-        if ($location.Key -eq "Start Menu" -and -not (Test-Path -Path $location.Value)) {
-            New-Item -Path $location.Value -ItemType Directory | Out-Null
-        }
-        $shortcutPath = [System.IO.Path]::Combine($location.Value, "KimDog's Windows Utility.lnk")
-        
-        # Check if the shortcut already exists
-        if (-not (Test-Path -Path $shortcutPath)) {
-            Create-Shortcut -ShortcutName "KimDog's Windows Utility" -ShortcutPath $shortcutPath -TargetPath $shell -Arguments $shellArgs -RunAsAdmin $true
-        } else {
-            Write-Host "Shortcut '$shortcutPath' already exists." -ForegroundColor Yellow
-        }
-    }
-}
-
 # Add this function to fetch URLs from JSON
 function Get-URLsFromJson {
     param (
@@ -156,6 +100,3 @@ if ($urls) {
         }
     }
 }
-
-# Call the shortcut creation function
-Create-WinUtilShortcuts

0e5f2a461e8383a957d57138ce23ad6930bf8e44
KimDog Studios <kim.dog.543@gmail.com>
2024-09-18 23:27:26 +0100

    Update

--- functions/WindowsUtility/WPFWindowsManager.ps1
+++ functions/WindowsUtility/WPFWindowsManager.ps1
@@ -12,11 +12,22 @@ try {
 }
 
 # Access the URLs from the parsed JSON
-$removeAppXFilesUrl = $urls.urls.WPFRemoveAppX.URL
-$ultimatePerformanceUrl = $urls.urls.WPFUltimatePerformance.URL
-$darkModeUrl = $urls.urls.InvokeDarkMode.URL
-$mouseAccelerationUrl = $urls.urls.InvokeMouseAcceleration.URL
-$gamingOptimizationUrl = $urls.urls.WPFGamingOptimization.URL
+$urlsToAccess = @(
+    "WPFRemoveAppX", # Removes Bloatware from the System
+    "WPFUltimatePerformance", # Adds and Enbales Ultimate Performance Power Scheme
+    "InvokeDarkMode", # Sets PC Theme to Dark Mode
+    "InvokeMouseAcceleration", # Disbales Mouse Acceleration
+    "WPFGamingOptimization", # Enables Gaming Optimizations
+    "UninstallWinUtilEdgeBrowser", # Uninstall Microsoft Edge
+    "InvokeSetWindowsUpdatesToDefault", # Sets Windows Updates to Default
+    "InvokeSetWindowsUpdatesToDisabled", # Sets Windows Updates to Disabled
+    "InvokeSetWindowsUpdatesToSecurity", # Sets Windows Updates to Security
+    "WPFClassicRightClick" # Enables Classic Right Click Menu [Right Clicking on Windows 11]
+)
+
+foreach ($urlKey in $urlsToAccess) {
+    Set-Variable -Name "${urlKey}Url" -Value $urls.urls."$urlKey".URL
+}
 
 # Function to fetch and execute the script from the URL
 function Run-ScriptFromUrl {
@@ -47,13 +58,21 @@ function Read-Key {
 
 # Define menu options and their corresponding actions
 $menuOptions = @(
-    @{ Name = "Optimize for Gaming [Runs Options: 3, 4, 5]"; Action = { Option1 } },
-    @{ Name = "Remove Bloatware [Windows 11 Only]"; Action = { Option2 } },
-    @{ Name = "Add & Apply Ultimate Performance Mode"; Action = { Option3 } },
-    @{ Name = "Apply Dark Mode to Windows"; Action = { Option4 } },
-    @{ Name = "Disable Mouse Acceleration"; Action = { Option5 } },
+    @{ Name = "Optimize for Gaming [Runs Options: 3, 4, 5]"; Action = { Run-ScriptFromUrl -Url $gamingOptimizationUrl } },
+    @{ Name = "Remove Bloatware [Windows 11 Only]"; Action = { Run-ScriptFromUrl -Url $removeAppXFilesUrl } },
+    @{ Name = "Add & Apply Ultimate Performance Mode"; Action = { Run-ScriptFromUrl -Url $ultimatePerformanceUrl } },
+    @{ Name = "Apply Dark Mode to Windows"; Action = { Run-ScriptFromUrl -Url $darkModeUrl } },
+    @{ Name = "Disable Mouse Acceleration"; Action = { Run-ScriptFromUrl -Url $mouseAccelerationUrl } },
+    @{ Name = "Remove Microsoft Edge"; Action = { Run-ScriptFromUrl -Url $UninstallWinUtilEdgeBrowser } },
+    @{ Name = "Set Windows Updates to Default"; Action = { Run-ScriptFromUrl -Url $setWindowsUpdatesToDefault } },
+    @{ Name = "Set Windows Updates to Disabled"; Action = { Run-ScriptFromUrl -Url $setWindowsUpdatesToDisabled } },
+    @{ Name = "Set Windows Updates to Security"; Action = { Run-ScriptFromUrl -Url $setWindowsUpdatesToSecurity } },
+    @{ Name = "Enable Classic Right Click Menu [Right Clicking on Windows 11]"; Action = { Run-ScriptFromUrl -Url $classicRightClickUrl } },
     @{ Name = "Exit"; Action = { Write-Host "Exiting..." -ForegroundColor Red; exit } }
 )
+
+# Sort menu options alphabetically by Name
+$menuOptions = $menuOptions | Sort-Object { $_.Name }
 
 $currentIndex = 0  # Track the current index
 
@@ -74,46 +93,6 @@ function Show-MainMenu {
         }
     }
     "`n" | Out-String  # Explicitly add a line break after each option
-}
-
-# Function for Option 1: Optimize for Gaming
-function Option1 {
-    Write-Host "You selected Option 1: Optimize for Gaming" -ForegroundColor Green
-    Run-ScriptFromUrl -Url $gamingOptimizationUrl
-    Write-Host "`nPress Enter to return to the main menu..." -ForegroundColor Cyan
-    Read-Host
-}
-
-# Function for Option 2: Remove Bloatware
-function Option2 {
-    Write-Host "You selected Option 2: Remove Bloatware" -ForegroundColor Green
-    Run-ScriptFromUrl -Url $removeAppXFilesUrl
-    Write-Host "`nPress Enter to return to the main menu..." -ForegroundColor Cyan
-    Read-Host
-}
-
-# Function for Option 3: Add & Apply Ultimate Performance Mode
-function Option3 {
-    Write-Host "You selected Option 3: Add & Apply Ultimate Performance Mode" -ForegroundColor Green
-    Run-ScriptFromUrl -Url $ultimatePerformanceUrl
-    Write-Host "`nPress Enter to return to the main menu..." -ForegroundColor Cyan
-    Read-Host
-}
-
-# Function for Option 4: Apply Dark Mode to Windows
-function Option4 {
-    Write-Host "You selected Option 4: Apply Dark Mode to Windows" -ForegroundColor Green
-    Run-ScriptFromUrl -Url $darkModeUrl
-    Write-Host "`nPress Enter to return to the main menu..." -ForegroundColor Cyan
-    Read-Host
-}
-
-# Function for Option 5: Disable Mouse Acceleration
-function Option5 {
-    Write-Host "You selected Option 5: Disable Mouse Acceleration" -ForegroundColor Green
-    Run-ScriptFromUrl -Url $mouseAccelerationUrl
-    Write-Host "`nPress Enter to return to the main menu..." -ForegroundColor Cyan
-    Read-Host
 }
 
 # Main loop
